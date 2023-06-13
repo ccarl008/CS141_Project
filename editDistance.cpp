e74c52f3aa435a2800a6dcbb8868378ab3b51385
ccarl008 <89361377+ccarl008@users.noreply.github.com>
2023-06-12 21:27:28 -0700

    Update editDistance.cpp

--- editDistance.cpp
+++ editDistance.cpp
@@ -3,12 +3,14 @@
 #include <string>
 #include <algorithm>
 
-int EditDistance(const std::string& x, const std::string& y) {
+using namespace std;
+
+int EditDistance(const string& x, const string& y) {
     int m = x.length();
     int n = y.length();
 
     // Create a table to store the edit distances
-    std::vector<std::vector<int>> dp(m + 1, std::vector<int>(n + 1, 0));
+    vector<vector<int> > dp(m + 1, vector<int>(n + 1, 0));
 
     // Initialize the table with base cases
     for (int i = 0; i <= m; ++i) {
@@ -24,47 +26,47 @@ int EditDistance(const std::string& x, const std::string& y) {
             if (x[i - 1] == y[j - 1]) {
                 dp[i][j] = dp[i - 1][j - 1];
             } else {
-                dp[i][j] = std::min({dp[i - 1][j], dp[i][j - 1], dp[i - 1][j - 1]}) + 1;
+                dp[i][j] = min(min(dp[i - 1][j], dp[i][j - 1]), dp[i - 1][j - 1]) + 1;
             }
         }
     }
 
     // Print the sequence of operations
     int i = m, j = n;
-    std::vector<std::string> operations;
+    vector<string> operations;
     while (i > 0 && j > 0) {
         if (x[i - 1] == y[j - 1]) {
             --i;
             --j;
         } else if (dp[i][j] == dp[i - 1][j - 1] + 1) {
-            operations.push_back("Substitute \"" + std::string(1, x[i - 1]) + "\" into \"" + std::string(1, y[j - 1]) + "\"");
+            operations.push_back("Substitute \"" + string(1, x[i - 1]) + "\" into \"" + string(1, y[j - 1]) + "\"");
             --i;
             --j;
         } else if (dp[i][j] == dp[i - 1][j] + 1) {
-            operations.push_back("Delete \"" + std::string(1, x[i - 1]) + "\"");
+            operations.push_back("Delete \"" + string(1, x[i - 1]) + "\"");
             --i;
         } else if (dp[i][j] == dp[i][j - 1] + 1) {
-            operations.push_back("Insert \"" + std::string(1, y[j - 1]) + "\"");
+            operations.push_back("Insert \"" + string(1, y[j - 1]) + "\"");
             --j;
         }
     }
 
     while (i > 0) {
-        operations.push_back("Delete \"" + std::string(1, x[i - 1]) + "\"");
+        operations.push_back("Delete \"" + string(1, x[i - 1]) + "\"");
         --i;
     }
 
     while (j > 0) {
-        operations.push_back("Insert \"" + std::string(1, y[j - 1]) + "\"");
+        operations.push_back("Insert \"" + string(1, y[j - 1]) + "\"");
         --j;
     }
 
     // Reverse the vector of operations to get the correct order
-    std::reverse(operations.begin(), operations.end());
+    reverse(operations.begin(), operations.end());
 
     // Print the operations
-    for (const auto& operation : operations) {
-        std::cout << operation << std::endl;
+    for (size_t k = 0; k < operations.size(); ++k) {
+        cout << operations[k] << endl;
     }
 
     // Return the minimum edit distance
@@ -72,10 +74,10 @@ int EditDistance(const std::string& x, const std::string& y) {
 }
 
 int main() {
-    std::string x = "clarks";
-    std::string y = "kirk";
+    string x = "clarks";
+    string y = "kirk";
     int distance = EditDistance(x, y);
-    std::cout << "Minimum edit distance: " << distance << std::endl;
+    cout << "Minimum edit distance: " << distance << endl;
 
     return 0;
 }
